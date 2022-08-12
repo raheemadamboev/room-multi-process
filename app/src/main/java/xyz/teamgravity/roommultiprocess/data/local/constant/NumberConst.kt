3674edb46fe27c9b1d8cd46fea3f31c5df0e79e6
raheemadamboev <vividricemaryandmax@gmail.com>
2022-08-12 14:49:40 +0500

    constants for number database

---
+++ app/src/main/java/xyz/teamgravity/roommultiprocess/data/local/constant/NumberConst.kt
@@ -0,0 +1,21 @@
+package xyz.teamgravity.roommultiprocess.data.local.constant
+
+object NumberConst {
+
+    /**
+     * Database name
+     */
+    const val NAME = "database_number"
+
+    /**
+     * Database version
+     */
+    const val VERSION = 1
+
+    /**
+     * Table number
+     * Entity -> [xyz.teamgravity.roommultiprocess.data.local.entity.NumberEntity]
+     * Model -> [xyz.teamgravity.roommultiprocess.data.model.NumberModel]
+     */
+    const val TABLE_NUMBER = "table_number"
+}
