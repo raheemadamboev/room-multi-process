0d7970ecf560b8a844b791cd4ea1c2712ea52deb
raheemadamboev <vividricemaryandmax@gmail.com>
2022-08-12 15:01:03 +0500

    provide dependencies

---
+++ app/src/main/java/xyz/teamgravity/roommultiprocess/injection/provide/ApplicationModule.kt
@@ -0,0 +1,33 @@
+package xyz.teamgravity.roommultiprocess.injection.provide
+
+import android.app.Application
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import xyz.teamgravity.roommultiprocess.data.local.constant.NumberConst
+import xyz.teamgravity.roommultiprocess.data.local.dao.NumberDao
+import xyz.teamgravity.roommultiprocess.data.local.database.NumberDatabase
+import xyz.teamgravity.roommultiprocess.data.repository.NumberRepository
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ApplicationModule {
+
+    @Provides
+    @Singleton
+    fun provideNumberDatabase(application: Application): NumberDatabase =
+        Room.databaseBuilder(application, NumberDatabase::class.java, NumberConst.NAME)
+            .fallbackToDestructiveMigration()
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideNumberDao(numberDatabase: NumberDatabase): NumberDao = numberDatabase.numberDao()
+
+    @Provides
+    @Singleton
+    fun provideNumberRepository(numberDao: NumberDao): NumberRepository = NumberRepository(numberDao)
+}
