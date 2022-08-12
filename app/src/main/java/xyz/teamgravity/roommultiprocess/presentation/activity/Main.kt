b8be65e7f1adf03522ba5d45bb7f97df0eeef44f
raheemadamboev <vividricemaryandmax@gmail.com>
2022-08-12 15:33:16 +0500

    setup main

--- app/src/main/java/xyz/teamgravity/roommultiprocess/presentation/activity/Main.kt
+++ app/src/main/java/xyz/teamgravity/roommultiprocess/presentation/activity/Main.kt
@@ -1,5 +1,6 @@
 package xyz.teamgravity.roommultiprocess.presentation.activity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -7,9 +8,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.content.ContextCompat
+import dagger.hilt.android.AndroidEntryPoint
+import xyz.teamgravity.roommultiprocess.core.service.NumberService
+import xyz.teamgravity.roommultiprocess.presentation.screen.NumberScreen
 import xyz.teamgravity.roommultiprocess.presentation.theme.RoomMultiProcessTheme
 
+@AndroidEntryPoint
 class Main : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -18,9 +25,19 @@ class Main : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    NumberScreen()
                 }
             }
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        startService(Intent(this, NumberService::class.java))
+    }
+
+    override fun onPause() {
+        super.onPause()
+        stopService(Intent(this, NumberService::class.java))
     }
 }
